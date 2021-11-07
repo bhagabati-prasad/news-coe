e3f756fde143e5e54ac777a74cd781a6121208b2
Ayushmina-20 <ayushminanayak20@gmail.com>
2021-11-06 20:11:53 -0700

    changes

--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -1,65 +1,55 @@
 import React, { useEffect, useState } from "react";
 import Navigation from "./Navigation";
-import Catcard from "./Catcard"
+import Catcard from "./Catcard";
 import { Row, Col, Container } from "react-bootstrap";
 const Category = () => {
-   console.log('hello category')
-    const [users, setUsers] = useState([])
-    const [word, setWord] = useState("")
-    let urlParams = new URLSearchParams(window.location.search);
-    let myParam = urlParams.get('q');
-    console.log(myParam)
-    useEffect(()=>{
-       
-       setWord(myParam)
-console.log('hello from use',word)
-    fetch("/all",{
-        method:'POST',
-        headers:{
-            "Content-type":"application/json"
-        },
-        body:JSON.stringify({
-            myParam
-        })
+  console.log("hello category");
+  const [users, setUsers] = useState([]);
+  const [word, setWord] = useState("");
+  let urlParams = new URLSearchParams(window.location.search);
+  let myParam = urlParams.get("q");
+  console.log(myParam);
+  useEffect(() => {
+    setWord(myParam);
+    console.log("hello from use", word);
+    fetch("https://newerver.herokuapp.com/all", {
+      method: "POST",
+      headers: {
+        "Content-type": "application/json",
+      },
+      body: JSON.stringify({
+        myParam,
+      }),
     })
-    .then(res=>res.json())
-    .then(data=>{
-        if(data.length>0){
-            console.log(data)
-        setUsers(data)
-
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.length > 0) {
+          console.log(data);
+          setUsers(data);
+        } else {
+          alert("somethng went wrong with server , check once  !");
         }
-        else{
-            alert('somethng went wrong with server , check once  !')
-        }
+      })
+      .catch((err) => console.log("something went wrong with server"));
+  }, [word]);
 
-
-    })
-    .catch(err=>console.log('something went wrong with server')
-    )},[word])
-    
-
-    return (
-        <div>
-{users.length<1 ? console.log(users.length) :
+  return (
+    <div>
+      {users.length < 1 ? (
+        console.log(users.length)
+      ) : (
         <>
-
-            <Container fluid>
-                <Row>
-                    <Navigation />
-                </Row>
-                <Row>
-
-                    <Catcard users={users} /> 
-
-                </Row>
-
-
-            </Container>
-
+          <Container fluid>
+            <Row>
+              <Navigation />
+            </Row>
+            <Row>
+              <Catcard users={users} />
+            </Row>
+          </Container>
         </>
-}
-</div>
-    )
-}
-export default Category
+      )}
+    </div>
+  );
+};
+export default Category;
